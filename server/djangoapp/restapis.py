4019e37c07d2f03574fd5c0562c53beaf8e75d87
ctienon1 <tienoncelestin@gmail.com>
2024-05-10 19:44:01 +0000

    updateXXX

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,6 +1,6 @@
 # Uncomment the imports below before you add the function code
-import requests
 import os
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,8 +10,11 @@ sentiment_analyzer_url = os.getenv(
     "sentiment_analyzer_url", default="http://localhost:5050/"
 )
 
+
 # def get_request(endpoint, **kwargs):
 def get_request(endpoint, **kwargs):
+    """function"""
+
     params = ""
     if kwargs:
         for key, value in kwargs.items():
@@ -19,35 +22,39 @@ def get_request(endpoint, **kwargs):
 
     request_url = backend_url + endpoint + "?" + params
 
-    print("GET from {} ".format(request_url))
+    print(f"GET from {request_url}")
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except requests.RequestException as e:
         # If any error occurs
-        print("Network exception occurred")
+        print(f"Network exception occurred : {e}")
 
 
 def analyze_review_sentiments(text):
+    """function"""
+
     request_url = sentiment_analyzer_url + "analyze/" + text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except Exception as err:
+    except requests.RequestException as err:
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
 
 def post_review(data_dict):
+    """function"""
+
     request_url = backend_url + "/insert_review"
     try:
         response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
-        print("Network exception occurred")
+    except requests.RequestException as e:
+        print(f"Network exception occurred : {e}")
 
 
 # Add code for posting review
